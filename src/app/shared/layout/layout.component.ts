80659975fbb2c6321900ada66fa6d2a612a060f9
meszaroslaszlo <laszlo.meszaros.connect@gmail.com>
2025-05-14 18:44:23 +0200

    Inital layout.

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [
+    RouterModule,
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+currentYear: any;
+logout() {
+throw new Error('Method not implemented.');
+}
+userId: any;
+switchMenuSize() {
+throw new Error('Method not implemented.');
+}
 
 }
