a89df1a8d28c991393cf46e84b7a9c9bd79cfc5c
meszaroslaszlo <laszlo.meszaros.connect@gmail.com>
2025-05-14 18:25:26 +0200

    Inital components

---
+++ src/app/feature/recipe/recipedetail/recipedetail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipedetail',
+  imports: [],
+  templateUrl: './recipedetail.component.html',
+  styleUrl: './recipedetail.component.scss'
+})
+export class RecipedetailComponent {
+
+}
---
+++ src/app/feature/recipe/recipeform/recipeform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipeform',
+  imports: [],
+  templateUrl: './recipeform.component.html',
+  styleUrl: './recipeform.component.scss'
+})
+export class RecipeformComponent {
+
+}
